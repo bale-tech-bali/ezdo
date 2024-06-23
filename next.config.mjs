7c8751c32ade4ae8e9443f15c53ca38be089cc76
Arya Indra Tanaya <aryaindratanaya@gmail.com>
2024-06-23 13:15:22 +0800

    feat: :technologist: enable the use of `scss` into the project
    * add `sass` in `devDependencies`
    * update the `next.config.mjs` to support `sass`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+// Convert URL to path for __dirname equivalent
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: [path.join(__dirname, 'styles')],
+}
 
 export default nextConfig
