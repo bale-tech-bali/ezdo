d13472f14f63dcc6c168143a3921f021fa22e523
Arya Indra Tanaya <aryaindratanaya@gmail.com>
2024-06-25 01:04:40 +0800

    feat: :sparkles: implement permanent redirect at `/dashboard` to `/dashboard/home`

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: [path.join(__dirname, 'styles')],
+  async redirects() {
+    return [
+      {
+        source: '/dashboard',
+        destination: '/dashboard/home',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
